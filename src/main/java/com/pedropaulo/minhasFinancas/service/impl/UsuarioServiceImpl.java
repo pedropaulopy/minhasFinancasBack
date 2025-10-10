d294104296587dd79b95aecaa1096484f954e842
Pedro Paulo <pedro.carlo@muralis.com.br>
2025-10-10 13:56:04 -0300

    Implementa a estrutura inicial do UsuarioService.

--- src/main/java/com/pedropaulo/minhasFinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/pedropaulo/minhasFinancas/service/impl/UsuarioServiceImpl.java
@@ -1,4 +1,30 @@
 package com.pedropaulo.minhasFinancas.service.impl;
 
-public class UsuarioServiceImpl {
+import com.pedropaulo.minhasFinancas.service.UsuarioService;
+import model.entity.Usuario;
+import model.repository.UsuarioRepository;
+
+public class UsuarioServiceImpl implements UsuarioService {
+
+    private UsuarioRepository repository;
+
+    public UsuarioServiceImpl(UsuarioRepository repository) {
+        super();
+        this.repository = repository;
+    }
+
+    @Override
+    public Usuario autenticar(String email, String senha) {
+        return null;
+    }
+
+    @Override
+    public Usuario salvarUsuario(Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public void validarEmail(String email) {
+
+    }
 }
