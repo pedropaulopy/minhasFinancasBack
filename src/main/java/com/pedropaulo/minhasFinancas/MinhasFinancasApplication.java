e93391b98fa263804009d9a456a7b9a7fb8168f2
Pedro Paulo <pedro.carlo@muralis.com.br>
2025-10-17 16:07:13 -0300

    Permite acessos apenas de localhost:3000

--- src/main/java/com/pedropaulo/minhasFinancas/MinhasFinancasApplication.java
+++ src/main/java/com/pedropaulo/minhasFinancas/MinhasFinancasApplication.java
@@ -11,7 +11,7 @@ public class MinhasFinancasApplication implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 
