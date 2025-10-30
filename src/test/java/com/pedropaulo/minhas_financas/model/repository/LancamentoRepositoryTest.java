fc6e441789a3f6f17ec3818cf1a26d6a5073b20f
Pedro Paulo <pedro.carlo@muralis.com.br>
2025-10-30 09:25:50 -0300

    refactor importa Assertion junto com outros imports em LancamentoRepositoryTest e remove o tipo static desnecessário de criaUsuario em UsuarioRepositoryTest

--- src/test/java/com/pedropaulo/minhas_financas/model/repository/LancamentoRepositoryTest.java
+++ src/test/java/com/pedropaulo/minhas_financas/model/repository/LancamentoRepositoryTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -49,7 +50,7 @@ public class LancamentoRepositoryTest {
                 .build();
 
         Lancamento salvo = repository.save(lancamento);
-        org.assertj.core.api.Assertions.assertThat(salvo.getId()).isNotNull();
+        assertThat(salvo.getId()).isNotNull();
     }
 
     @Test
