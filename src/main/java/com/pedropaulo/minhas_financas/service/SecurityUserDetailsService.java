a33a7c40bb7dc1a770bd6cb25f570310d8f090dd
Pedro Paulo <pedro.carlo@muralis.com.br>
2025-10-30 00:36:54 -0300

    Adiciona a interface SecurityUserDetailsService estendendo UserDetailsService a fim de refletir melhor o padrao utilizado até o momento no projeto

---
+++ src/main/java/com/pedropaulo/minhas_financas/service/SecurityUserDetailsService.java
@@ -0,0 +1,6 @@
+package com.pedropaulo.minhas_financas.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface SecurityUserDetailsService extends UserDetailsService {
+}
--- src/main/java/com/pedropaulo/minhas_financas/service/impl/SecurityUserDetailsServiceImpl.java
+++ src/main/java/com/pedropaulo/minhas_financas/service/impl/SecurityUserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.pedropaulo.minhas_financas.service.impl;
 
 import com.pedropaulo.minhas_financas.model.entity.Usuario;
 import com.pedropaulo.minhas_financas.model.repository.UsuarioRepository;
+import com.pedropaulo.minhas_financas.service.SecurityUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SecurityUserDetailsServiceImpl implements UserDetailsService {
+public class SecurityUserDetailsServiceImpl implements SecurityUserDetailsService {
 
   private final UsuarioRepository usuarioRepository;
 
