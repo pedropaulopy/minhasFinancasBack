c3e726befdff95386837fcb9636e5fbc5fd5879b
Pedro Paulo <pedro.carlo@muralis.com.br>
2025-10-30 09:13:30 -0300

    refactor implementa um service SecurityUserDetails que estende UserDetailsService, a fim de refletir melhor o padrao utilizado no projeto

---
+++ src/main/java/com/pedropaulo/minhas_financas/service/SecurityUserDetailsService.java
@@ -0,0 +1,5 @@
+package com.pedropaulo.minhas_financas.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface SecurityUserDetailsService extends UserDetailsService {}
