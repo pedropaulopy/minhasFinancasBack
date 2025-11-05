848670c2308f4435e051b7f35457b7d7eb3feffa
Pedro Paulo <pedro.carlo@muralis.com.br>
2025-11-05 02:36:47 -0300

    move a classe auxiliar para a pasta utils

--- src/test/java/com/pedropaulo/minhas_financas/service/testUtils/StubUserDetailsService.java
+++ src/test/java/com/pedropaulo/minhas_financas/service/testUtils/StubUserDetailsService.java
@@ -3,13 +3,9 @@ package com.pedropaulo.minhas_financas.service.testUtils;
 import com.pedropaulo.minhas_financas.service.impl.SecurityUserDetailsServiceImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public class StubUserDetailsService extends SecurityUserDetailsServiceImpl {
-
-    public StubUserDetailsService() {
-        super(null);
-    }
-
+public class StubUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) {
         return User.withUsername(username).password("123").roles("USER").build();
